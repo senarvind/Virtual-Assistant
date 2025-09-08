e21fa3b748343da7a8d3ff854ac021d4d2dc91b5
senarvind <senarvind6264@gmail.com>
2025-09-08 08:31:41 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -26,7 +26,7 @@ export const signUp = async (req, res)=>{
     res.cookie("token", token, {
   httpOnly: true,
   maxAge: 7 * 24 * 60 * 60 * 1000, 
-  sameSite: "none",
+  sameSite: "None",
   secure: true 
 });
 
@@ -59,7 +59,7 @@ export const Login = async (req, res)=>{
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "none",
+      sameSite: "None",
       secure: true
     });
 
