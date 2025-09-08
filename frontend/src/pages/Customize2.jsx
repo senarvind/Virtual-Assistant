93a9b497f9ef8d38e8be4b58969c3875c7ab0564
senarvind <senarvind6264@gmail.com>
2025-09-08 08:11:45 +0530

    Update Customize2.jsx

--- frontend/src/pages/Customize2.jsx
+++ frontend/src/pages/Customize2.jsx
@@ -21,7 +21,7 @@ const Customize2 = () => {
         else{
           formData.append("imageUrl", selectedImage);
         }
-        const result = await axios.post(`http://localhost:8000/api/user/update`, formData,{withCredentials:true})
+        const result = await axios.post(`https://virtual-assistant-backend-zv2l.onrender.com`, formData,{withCredentials:true})
         console.log(result.data);
         setUserData(result.data);
         navigate("/")
